bfbefb09668fd5daa351fbc1daa881d9e7cc9294
javitocor <javitocor@gmail.com>
2021-09-30 16:30:51 +0700

    implement category model

---
+++ models/category.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var CategorySchema = new Schema({
+    name : { type: String, required: true, unique: true },
+});
+
+
+module.exports = mongoose.model('Category', CategorySchema);
