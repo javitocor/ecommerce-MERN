b377ad9789e010ec98c0082838c68572b5b90aef
javitocor <javitocor@gmail.com>
2021-09-30 16:18:34 +0700

    implement category routes

---
+++ routes/category.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var upload = require('../middleware/imageUpload');
+const validations = require('../middleware/validations');
+
+var category_controller = require('../controllers/categoryController');
+
+router.get('/', category_controller.category_list);
+router.get('/:id', category_controller.category_detail);
+router.post('/create',   category_controller.category_create);
+router.put('/:id',   category_controller.category_update);
+router.delete('/:id', category_controller.category_delete);
+
+module.exports = router;
