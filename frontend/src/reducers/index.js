d8dbed8a3bd6a65eaae0ae665f069ef49f1e7bba
javitocor <javitocor@gmail.com>
2021-10-01 01:51:29 +0700

    add reducers to rootreducere

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,6 +1,11 @@
 import { combineReducers } from 'redux';
+import authReducer from './auth';
+import messageReducer from './message';
+
 
 const rootReducer = combineReducers({
+  auth: authReducer,
+  message: messageReducer,
 });
 
 export default rootReducer;
