48a89dd89df801d58561721a14b31aa63ae61883
javitocor <javitocor@gmail.com>
2021-09-30 17:50:00 +0700

    implement shippingaddress controller

---
+++ controllers/shippingAddressController.js
@@ -0,0 +1,78 @@
+const ShippingAddress = require('../models/shippingAddress');
+
+const { validationResult } = require("express-validator");
+
+exports.shippingAddress_list = async (req, res, next) => {
+  try {
+    const shippingAddresss = await ShippingAddress.find({}).select("-__v");
+    res.json(shippingAddresss);
+  } catch (error) {
+    res.json(error)
+    next();
+  }
+};
+
+exports.shippingAddress_detail = async (req, res, next) => {
+  try {
+    const shippingAddress = await ShippingAddress.findById(req.params.id).select("-__v").populate('customer');
+    res.json(shippingAddress);
+  } catch (error) {
+    res.json(error)
+    next();
+  }
+};
+
+exports.shippingAddress_create = async (req, res, next) => { 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array()
+      });
+    };
+
+    try {
+      const {customer, order, address, city, state, zipcode} = req.body;
+      const shippingAddress = new ShippingAddress({
+        customer,
+        order,
+        address,
+        city,
+        state,
+        zipcode,
+      });
+      await shippingAddress.save();
+      res.status(201);
+      res.json({message: 'ShippingAddress created successfully', shippingAddress});
+    } catch (error) {
+      res.json(error)
+      next();
+    }
+};
+
+exports.shippingAddress_update = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array()
+      });
+    };
+    try {
+      const shippingAddress = await ShippingAddress.findByIdAndUpdate(req.params.id, { $set: req.body, updated_at: Date.now() }, {new: true});
+      res.status(200);
+      res.json({message: 'ShippingAddress updated successfully', shippingAddress});
+    } catch (error) {
+      console.log(error)
+      next();
+    }
+};
+
+exports.shippingAddress_delete = async (req, res, next) => {
+  try {
+    await ShippingAddress.findByIdAndDelete(req.params.id);
+    res.status(200);
+    res.json({message: 'ShippingAddress deleted Successfully'});
+  } catch (error) {
+    res.json(error)
+    next();
+  }
+};
