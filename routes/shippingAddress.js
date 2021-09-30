2a2e12cfe51c7da55a14a73976200f0c5b062ed6
javitocor <javitocor@gmail.com>
2021-09-30 16:08:37 +0700

    set up shippingaddress routes

---
+++ routes/shippingAddress.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var upload = require('../middleware/imageUpload');
+const validations = require('../middleware/validations');
+
+var shippingAddress_controller = require('../controllers/shippingAddressController');
+
+router.get('/', shippingAddress_controller.shippingAddress_list);
+router.get('/:id', shippingAddress_controller.shippingAddress_detail);
+router.post('/create',   shippingAddress_controller.shippingAddress_create);
+router.put('/:id',   shippingAddress_controller.shippingAddress_update);
+router.delete('/:id', shippingAddress_controller.shippingAddress_delete);
+
+module.exports = router;
