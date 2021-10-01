f7e6bfb05d9a6ef7db5b3f87e8e888eea45a58a4
javitocor <javitocor@gmail.com>
2021-10-01 13:19:49 +0700

    combine reducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,11 +1,25 @@
 import { combineReducers } from 'redux';
 import authReducer from './auth';
 import messageReducer from './message';
+import customerReducer from './customers';
+import productReducer from './products';
+import roleReducer from './roles';
+import categoryReducer from './categories';
+import orderReducer from './orders';
+import orderItemReducer from './orderItems';
+import shippingAddressReducer from './shippingAddress';
 
 
 const rootReducer = combineReducers({
   auth: authReducer,
   message: messageReducer,
+  customers: customerReducer,
+  products: productReducer,
+  roles: roleReducer,
+  categories: categoryReducer,
+  orders: orderReducer,
+  orderItems: orderItemReducer,
+  shippingAddress: shippingAddressReducer,
 });
 
 export default rootReducer;
