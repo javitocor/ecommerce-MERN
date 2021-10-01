b3749506f4d1ba79dde9d0fd449c841426c9aed2
javitocor <javitocor@gmail.com>
2021-10-01 13:55:31 +0700

    add riole to login payload

--- controllers/authController.js
+++ controllers/authController.js
@@ -35,9 +35,11 @@ exports.login_post = async (req, res, next) => {
       expiresIn: 631139040 // 20 years
     });
     
+    const authorities = 'ROLE_' + customer.role.name;
 
     res.status(200).json({
       customer,
+      role: authorities,
       accessToken: token
     });
   } catch (error) {
