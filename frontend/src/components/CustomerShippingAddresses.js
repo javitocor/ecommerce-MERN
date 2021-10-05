a459c0771480322db65c6e10ef12418c3cce7d44
javitocor <javitocor@gmail.com>
2021-10-05 12:15:25 +0700

    implement customershippingaddresses component

---
+++ frontend/src/components/CustomerShippingAddresses.js
@@ -0,0 +1,93 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
+
+const CustomerShippingAddresses = props => {
+  const {addresses, remove} = props;
+
+  function handleDelete(addressId) {
+    remove('shippingAddress', addressId)
+  }
+
+  return (
+    <div className="container-fluid">
+      <div className="form cf mt-5">
+        <section className="plan cf d-flex flex-column">
+          {addresses.map(address => (
+            <>
+              <input type="radio" name="radio1" id="free" value="free" />
+              <label className="free-label d-flex flex-row justify-content-between align-items-center" htmlFor="free">
+                <div className="left">Basic</div>
+                <div className="center">
+                  <div className="row text-left h-100">
+                    <div className="col-3">
+                      Address:
+                    </div>
+                    <div className="col-9">
+                      {address.address}
+                    </div>
+                    <div className="col-3">
+                      City:
+                    </div>
+                    <div className="col-9">
+                      {address.city}
+                    </div>
+                    <div className="col-3">
+                      State:
+                    </div>
+                    <div className="col-9">
+                      {address.state}
+                    </div>
+                    <div className="col-3">
+                      Country:
+                    </div>
+                    <div className="col-9">
+                      {address.country}
+                    </div>
+                    <div className="col-3">
+                      Zip Code:
+                    </div>
+                    <div className="col-9">
+                      {address.zipcode}
+                    </div>
+                  </div>
+                </div>
+                <div className="right">
+                  <Link
+                    to={{
+                        pathname: `/shippingaddress/${address.name}/edit`,
+                        state: { address }
+                      }}
+                    className="btn btn-primary"
+                    id="list-home-list"
+                    data-toggle="list"
+                    role="tab"
+                    aria-controls="home"
+                  >
+                    <i className="far fa-edit" />
+                    Edit
+                  </Link>
+                  <div className="btn btn-danger" onClick={handleDelete(address._id)}>
+                    <i className="fas fa-trash-alt" />
+                    {' '}
+                    Delete
+                  </div>
+                </div>
+              </label>
+            </>
+        ))}
+        </section>	
+      </div>
+    </div>
+  );
+};
+
+CustomerShippingAddresses.propTypes = {
+  addresses: PropTypes.arrayOf(PropTypes.object).isRequired,
+  remove: PropTypes.func.isRequired,
+};
+
+export default CustomerShippingAddresses;
--- frontend/src/containers/Profile.js
+++ frontend/src/containers/Profile.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/forbid-prop-types */
 /* eslint-disable jsx-a11y/label-has-associated-control */
@@ -6,13 +7,13 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Spinner from 'react-bootstrap/Spinner';
-import {shippingAddressByCustomer, ordersByCustomer, SingleCall} from '../helpers/apiCalls';
+import {shippingAddressByCustomer, ordersByCustomer, SingleCall, DeleteCall} from '../helpers/apiCalls';
 import CustomerOrders from '../components/CustomerOrders';
 import CustomerShippingAddress from '../components/CustomerShippingAddresses';
 import style from '../style/Profile.module.css';
 
 const Profile = props => {
-  const {location, auth, orders, shippingAddress, getOrders, getShipping, getCustomer } = props;
+  const {location, auth, orders, shippingAddress, getOrders, getShipping, getCustomer, deleteInstance } = props;
   const {id} = location.state;
   const {customer} = auth;
   const {ordersList} = orders;
@@ -30,94 +31,114 @@ const Profile = props => {
     })();
   }, []);
 
-  return customer === undefined ? <div className="d-flex justify-content-center align-items-center w-100"><Spinner animation="grow" /></div> : (
-    <div className={`container ${style.empprofile}`}>
-      <div>
-        <div className="row">
-          <div className="col-md-4">
-            <div className={style.profileimg}>
-              <img src="https://bootdey.com/img/Content/user_3.jpg" alt="" />
+  const getCookie = (name) => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (`${name  }=`)) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+  }
+
+  async function removeInstance(route, instanceId) {
+    const token = getCookie('csrftoken');
+    await deleteInstance(route, token, instanceId)
+  }
+
+    return customer === undefined ? <div className="d-flex justify-content-center align-items-center w-100"><Spinner animation="grow" /></div> : (
+      <div className={`container ${style.empprofile}`}>
+        <div>
+          <div className="row">
+            <div className="col-md-4">
+              <div className={style.profileimg}>
+                <img src="https://bootdey.com/img/Content/user_3.jpg" alt="" />
                         
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className={style.profilehead}>
-              <h5>
-                {customer.customer.username}
-              </h5>
-              <h6>
-                Since: 
-                {' '}
-                {customer.customer.created_at}
-              </h6>
-              <ul className="nav nav-tabs" id="myTab" role="tablist">
-                <li className="nav-item">
-                  <a className="nav-link active" id="orders-tab" data-toggle="tab" href="#orders" role="tab" aria-controls="orders" aria-selected="true">Orders</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" id="shipping-tab" data-toggle="tab" href="#shipping" role="tab" aria-controls="shipping" aria-selected="false">Shipping Addresses</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-md-2">
-            <input type="submit" className={style.profileeditbtn} name="btnAddMore" value="Edit Profile" />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-4">
-            <div className={style.profilework}>
-              <div className="row">
-                <div className="col-md-6">
-                  <label>Name</label>
-                </div>
-                <div className="col-md-6">
-                  <p>
-                    {customer.customer.firstname} 
-                    {' '}
-                    {customer.customer.lastname}
-                  </p>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-md-6">
-                  <label>Email</label>
-                </div>
-                <div className="col-md-6">
-                  <p>{customer.customer.email}</p>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-md-6">
-                  <label>Phone</label>
-                </div>
-                <div className="col-md-6">
-                  <p>{customer.customer.phone}</p>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-md-6">
-                  <label>Role</label>
-                </div>
-                <div className="col-md-6">
-                  <p>{customer.role}</p>
-                </div>
               </div>
             </div>
-          </div>
-          <div className="col-md-8">
-            <div className={`tab-content ${style.profiletab}`} id="myTabContent">
-              <div className="tab-pane fade show active" id="orders" role="tabpanel" aria-labelledby="orders-tab">
-                <CustomerOrders orders={ordersList} />
+            <div className="col-md-6">
+              <div className={style.profilehead}>
+                <h5>
+                  {customer.customer.username}
+                </h5>
+                <h6>
+                  Since: 
+                  {' '}
+                  {customer.customer.created_at}
+                </h6>
+                <ul className="nav nav-tabs" id="myTab" role="tablist">
+                  <li className="nav-item">
+                    <a className="nav-link active" id="orders-tab" data-toggle="tab" href="#orders" role="tab" aria-controls="orders" aria-selected="true">Orders</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" id="shipping-tab" data-toggle="tab" href="#shipping" role="tab" aria-controls="shipping" aria-selected="false">Shipping Addresses</a>
+                  </li>
+                </ul>
               </div>
-              <div className="tab-pane fade" id="shipping" role="tabpanel" aria-labelledby="shipping-tab">
-                <CustomerShippingAddress addresses={shippingList} />
+            </div>
+            <div className="col-md-2">
+              <input type="submit" className={style.profileeditbtn} name="btnAddMore" value="Edit Profile" />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <div className={style.profilework}>
+                <div className="row">
+                  <div className="col-md-6">
+                    <label>Name</label>
+                  </div>
+                  <div className="col-md-6">
+                    <p>
+                      {customer.customer.firstname} 
+                      {' '}
+                      {customer.customer.lastname}
+                    </p>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <label>Email</label>
+                  </div>
+                  <div className="col-md-6">
+                    <p>{customer.customer.email}</p>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <label>Phone</label>
+                  </div>
+                  <div className="col-md-6">
+                    <p>{customer.customer.phone}</p>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <label>Role</label>
+                  </div>
+                  <div className="col-md-6">
+                    <p>{customer.role}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-8">
+              <div className={`tab-content ${style.profiletab}`} id="myTabContent">
+                <div className="tab-pane fade show active" id="orders" role="tabpanel" aria-labelledby="orders-tab">
+                  <CustomerOrders orders={ordersList} remove={removeInstance} />
+                </div>
+                <div className="tab-pane fade" id="shipping" role="tabpanel" aria-labelledby="shipping-tab">
+                  <CustomerShippingAddress addresses={shippingList} remove={removeInstance} />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>           
-    </div>
+        </div>           
+      </div>
   );
 };
 
@@ -142,6 +163,7 @@ Profile.propTypes = {
   getOrders: PropTypes.func.isRequired,
   getShipping: PropTypes.func.isRequired,
   getCustomer: PropTypes.func.isRequired,
+  deleteInstance: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
@@ -165,6 +187,7 @@ const mapDispatchToProps = dispatch => bindActionCreators({
   getOrders: ordersByCustomer,
   getShipping: shippingAddressByCustomer,
   getCustomer: SingleCall,
+  deleteInstance: DeleteCall,
 }, dispatch);
 
 connect(mapStateToProps, mapDispatchToProps)(Profile);
